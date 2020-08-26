6340c1fab3533cbc12f269f1609f7ef8071e61fe
Yilin Chai <chaiy1@Yilins-MacBook-Pro.local>
2020-08-26 14:26:53 +1000

    mix import with require

--- src/level1/level2/level3/test.section.js
+++ src/level1/level2/level3/test.section.js
@@ -1,7 +1,10 @@
+import _ from 'lodash'
+
 module.exports = x => {
   describe('level 3.function block', () => {
     beforeEach(() => console.log('3 - beforeEach'))
     afterEach(() => console.log('3 - afterEach'))
+    console.log(_)
     test('', () => console.log('level 3 - function call', x))
   })
 }
--- src/level1/level2/test.section.js
+++ src/level1/level2/test.section.js
@@ -2,9 +2,11 @@ module.exports = x => {
   describe('level 2.function block', () => {
     beforeEach(() => console.log('2 - beforeEach'))
     afterEach(() => console.log('2 - afterEach'))
-    test('', () => console.log('level 2 - function call', x))
+    test('level2 test', () => console.log('level 2 - function call', x))
     require('./level3/test.section')(3)
-
+    // (async () => {
+    //   await import('./level3/test.section')
+    // })();
   })
 }
 
